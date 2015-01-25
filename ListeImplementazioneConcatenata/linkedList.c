b55b5d5318304fbba6e4591f8926c05ff726a558
Federico Braconi <federicobraconi3@gmail.com>
2015-01-25 23:32:07 +0100

    Prima commit

--- ListeImplementazioneConcatenata/linkedList.c
+++ ListeImplementazioneConcatenata/linkedList.c
@@ -3,92 +3,168 @@
 
 /*
  * Inserts the new key at the beginning of the list.
- * Returns the updated pointer to the first element of the list.
+ * Returns the updated pointer to the first nodo of the list.
  */
-LLElement * LLInsertAtBeginning(LLElement * first, int key) {
-    // TODO To be implemented
-    return NULL;
+LLElement * LLInsertAtBeginning(LLElement * first, int key){
+    LLElement * nodo;
+    nodo=(LLElement*)malloc(sizeof(LLElement));
+    if(nodo!=NULL)
+    {
+        nodo->next=first;
+        first=nodo;
+        nodo->key=key;
+    }
+    return (first);
 }
 
 /*
  * Inserts the new key at the end of the list.
- * Returns the updated pointer to the first element of the list.
+ * Returns the updated pointer to the first nodo of the list.
  */
-LLElement * LLInsertAtEnd(LLElement * first, int key) {
-    // TODO To be implemented
-    return NULL;
+LLElement * LLInsertAtEnd(LLElement * first, int key){
+    LLElement *nodo,**temp;
+    nodo=(LLElement*)malloc(sizeof(LLElement))
+    if(nodo!=NULL){
+	temp=&first;
+	nodo->next=NULL;
+        nodo->key=key;
+        while(*temp!=NULL){
+            temp=&((*temp)->next);
+	}
+        *temp=nodo;
+    }
+    return (first);
 }
 
 /*
  * Inserts the new key at the specified positon of the list.
- * Position is zero-based, meaning that the first element is at position 0,
+ * Position is zero-based, meaning that the first nodo is at position 0,
  * analogous to what happens in arrays.
  * 
- * Returns the updated pointer to the first element of the list.
+ * Returns the updated pointer to the first nodo of the list.
  */
-LLElement * LLInsertAtPosition(LLElement * first, int key, int position) {
-    // TODO To be implemented
-    return NULL;
+LLElement * LLInsertAtPosition(LLElement * first, int key, int position){
+    int i;
+    LLElement *nodo,**temp;
+    nodo=(LLElement*)malloc(sizeof(LLElement));
+    if(nodo!=NULL){
+	nodo->key = key;
+        temp=&first;
+        for(i=0;i<position;i++){
+            temp = &((*temp)->next);
+	}
+        nodo->next = *temp;
+        *temp = nodo;
+    }
+    return (first);
 }
 
 /*
  * Returns the size of the list.
  */
-int LLSize(LLElement * first) {
-    // TODO To be implemented
-    return 0;
-}   
+int LLSize(LLElement * first){
+    int dim;
+    while(first!=NULL){
+        first=first->next;
+	dim++;
+    }
+    return (dim);
+}
 
 /*
  * Returns the key at the specified position.
  */ 
-int LLGetKey(LLElement * first, int position) {
-    // TODO To be implemented
-    return 0;
+int LLGetKey(LLElement * first, int position){
+    int i;
+    for(i=0;i<position;i++)
+        first=first->next;
+    return (first->key);
 }
 
 /*
- * Returns the position of the first element, starting from startPosition, that
+ * Returns the position of the first nodo, starting from startPosition, that
  * has the specified key.
  * Returns -1 if not found. 
  */ 
-int LLFindKey(LLElement * first, int key, int startPosition) {
-    // TODO To be implemented
-    return -1;
+int LLFindKey(LLElement * first, int key, int startPosition){
+    int pos,i;
+    pos=-1;
+    for(i=0;i<startPosition;i++){
+        first=first->next;
+    }
+    for(i=i;position==-1 && first!=NULL;i++){
+        if(first->key==key){
+            position=counter;
+	}
+        first=first->next;
+    }
+    return (position);
 }
 
 /*
- * Remove the first element of the list.
- * Returns the updated pointer to the first element of the list.
+ * Remove the first nodo of the list.
+ * Returns the updated pointer to the first nodo of the list.
  */
-LLElement * LLRemoveFirst(LLElement * first) {
-    // TODO To be implemented
-    return NULL;
+LLElement * LLRemoveFirst(LLElement * first){
+    LLElement *nodoDel
+    if(first!=NULL){
+        nodoDel=first;
+        first=first->next;
+        free(nodoDel);
+    }
+    return (first);
 }
 
 /*
- * Remove the last element of the list.
- * Returns the updated pointer to the first element of the list.
+ * Remove the last nodo of the list.
+ * Returns the updated pointer to the first nodo of the list.
  */
-LLElement * LLRemoveLast(LLElement * first) {
-    // TODO To be implemented
-    return NULL;
+LLElement * LLRemoveLast(LLElement * first){
+    LLElement **nodoDel;
+    if(first!=NULL){
+        nodoDel=&first;
+        while((*nodoDel)->next!=NULL){
+            nodoDel = &((*nodoDel)->next);
+	}
+        free(*nodoDel);
+	*nodoDel = NULL;
+    }
+    return (first);
 }
 
 /*
- * Remove the element at the specified position.
- * Position is zero-based, meaning that the first element is at position 0,
+ * Remove the nodo at the specified position.
+ * Position is zero-based, meaning that the first nodo is at position 0,
  * analogous to what happens in arrays.
  * 
- * Returns the updated pointer to the first element of the list.
+ * Returns the updated pointer to the first nodo of the list.
  */
-LLElement * LLRemoveAtPosition(LLElement * first, int position) {
-    // TODO To be implemented
-    return NULL;
+LLElement * LLRemoveAtPosition(LLElement * first, int position){
+    int i;
+    LLElement *nodoDel,**temp;
+    if(first!=NULL)
+    {
+        temp=&first;
+        for(i=0;i<position;i++){
+            temp=&((*temp)->next);
+	}
+        nodoDel=*temp;
+        if((*temp)->next!=NULL){
+            *temp=(*temp)->next;
+	}
+        free(nodoDel);
+    }
+    return (first);
 }
 
 /* Empties the list */
-LLElement * LLEmptyList(LLElement *first) {
-    // TODO To be implemented
+LLElement * LLEmptyList(LLElement *first){
+    LLElement *nodoDel;
+    while(first!=NULL)
+    {
+        nodoDel=first;
+        first=first->next;
+        free(nodoDel);
+    }
     return NULL;
 }
